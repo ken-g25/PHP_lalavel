482f65d144793dc06a607325525e93f1d7c88177
”名前” <>
2020-08-16 05:26:41 +0000

    add new file

---
+++ php_04.php
@@ -0,0 +1,16 @@
+<?php
+    
+    function result ($i) {
+        echo $i * 2;
+    }
+    $i = 1;
+    result();
+    
+?>
+
+<?php 
+    function f($a,$b) {
+        return $a + $b ;
+    }
+     f();
+
